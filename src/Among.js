7b3166924659c9c2f876a8ff466e996eec2470bf
oleg <oleg@kogorta.dp.ua>
2012-12-23 18:56:09 +0200

    toCharArray() became  anonymous

--- src/Among.js
+++ src/Among.js
@@ -12,14 +12,12 @@ function Among(s, substring_i, result, method) {
 		throw ("Bad Among initialisation: s:" + s + ", substring_i: "
 				+ substring_i + ", result: " + result);
 	this.s_size = s.length;
-	this.s = this.toCharArray(s);
+	this.s = (function() {
+		var sLength = s.length, charArr = new Array(sLength);
+		for (var i = 0; i < sLength; i++)
+			charArr[i] = s.charCodeAt(i);
+		return charArr;})();
 	this.substring_i = substring_i;
 	this.result = result;
 	this.method = method;
-}
-Among.prototype.toCharArray = function(s) {
-	var sLength = s.length, charArr = new Array(sLength);
-	for (var i = 0; i < sLength; i++)
-		charArr[i] = s.charCodeAt(i);
-	return charArr;
 }
